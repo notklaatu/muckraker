c02d0ba99a74678f4b8b3769a60baa1d91815e58
Seth Kenlon <skenlon@redhat.com>
2019-05-15 14:55:06 +1200

    correct path to awk parser

--- muckraker.sh
+++ muckraker.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/bash
 # Seth Kenlon <skenlon@redhat.com>
 
@@ -8,6 +7,8 @@
 # This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+CWD=`readlink -f "${0}"`
 
 while [ True ]; do
 if [ "$1" = "" -o "$1" = "-h" -o "$1" = "--help" ]; then
@@ -37,7 +38,7 @@ FONT=/usr/share/fonts/redhat/RedHatDisplay-Regular.otf
 #FONT=overpass-regular
 TO=${TO:-5}
 FROM=${FROM:-9}
-SHARE=`pwd`/share
+SHARE="`dirname ${CWD}`"/share
 OUTPUT=${OUTPUT:-toc-"$SKU".pdf}
 set -e
 
